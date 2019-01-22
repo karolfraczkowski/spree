17fd744929ed2fabdba35abed5258e12b1c27c18
Karolfraczkowski <piotrek.pasciak@gmail.com>
2019-01-22 10:10:00 +0100

    secondpr

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source "https://rubygems.org"
 gem 'byebug'
 gem 'capybara'
 gem 'chromedriver-helper'
+gem 'faker'
 gem 'rspec'
 gem 'rspec-expectations'
 gem 'selenium-webdriver'
+gem "show_me_the_cookies"
--- spec/helpers/main_helper.rb
+++ spec/helpers/main_helper.rb
@@ -1,5 +1,4 @@
 module MainHelper
-
   def login(email, password)
     visit '/login'
 
@@ -7,10 +6,31 @@ module MainHelper
     fill_in 'spree_user_password', with: password
 
     click_button 'Login'
-        #find('.btn.btn-lg').click
+  end
+
+  def fill_in_credenntials(email, password)
+   fill_in 'spree_user_email', with: email
+   fill_in 'spree_user_password', with: password
   end
 
   def logout
     visit '/logout'
+  end
+
+  def signup(email, password, password_confirmation)
+    visit '/signup'
+
+    fill_in_credenntials(email, password)
+    fill_in 'spree_user_password_confirmation', with: password_confirmation
+
+    click_button 'Create'
+  end
+
+  def wait_for(seconds = 5)
+    begin
+      Selenium::WebDriver::Wait.new(timeout: seconds).until { yield }
+    rescue Selenium::WebDriver::Error::TimeOutError
+      false
+    end
   end
 end
